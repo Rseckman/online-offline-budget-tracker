30c11f20b2e304660876daee31106d54249ea0c8
Ryan <69523694+Rseckman@users.noreply.github.com>
2020-10-27 09:35:42 -0400

    changed last block of code on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -71,8 +71,15 @@ self.addEventListener("fetch", function(evt) {
   // if the request is not for the API, serve static assets using "offline-first" approach.
   // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
-    caches.match(evt.request).then(function(response) {
-      return response || fetch(evt.request);
+    fetch(evt.request).catch(function() {
+      return caches.match(evt.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
     })
   );
 });
